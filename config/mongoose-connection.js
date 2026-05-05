6929251a2bcb58595f3086e0690f2d62a3c0a533
Ehasanul Haque <ehasanmd365@gmail.com>
2026-05-05 11:16:21 +0600

    part 4 updated

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
+const dbgr = require('debug')('app:mongoose');
+const config = require('config'); 
+//const config = require('./development.json'); //same same but different
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/scatch")
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
+  // .connect(`${config.MONGODB_URI}/scatch`) //same same but different
   .then(function () {
-    console.log("connected");
+    // console.log("connected");
+    dbgr("connected");
   })
   .catch(function (err) {
     console.log(err);
+    dbgr("error connecting to MongoDB");
   });
 
   module.exports = mongoose.connection; //scatch database er whole connection export kore dilam, jate onno file e import kore use korte pari
+
+
